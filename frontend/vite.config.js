4980a24e3e26973b6688264db610a80755d34769
Tomiwa Adelae <thetommedia@gmail.com>
2024-04-11 23:52:55 +0100

    Made some edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// server: {
-	// 	proxy: {
-	// 		"/api": {
-	// 			// target: "https://jsonplaceholder.typicode.com",
-	// 			target: "http://localhost:5000",
-	// 			changeOrigin: true,
-	// 			rewrite: (path) => path.replace(/^\/api/, ""),
-	// 		},
-	// 	},
-	// },
 	plugins: [react()],
 });
